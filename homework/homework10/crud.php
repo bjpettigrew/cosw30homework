f19f0201361841de882c14f7b7337001c320ff0b
bjpettigrew <bjpettigrew@gmail.com>
2019-11-07 04:26:23 +0000

    correct errors and change error messages to under field box

--- homework/homework10/crud.php
+++ homework/homework10/crud.php
@@ -1,13 +1,13 @@
 <?php
 // Add the database connection
 
-include('includes/database.php');
+include('database.php');
 
 //make db info into constants
-define('DB_USERNAME_HW10', 'username');
-define('DB_PW_HW10', 'password');
-define('DB_HOSTNAME', 'hostname');
-define('DB_NAME_HW10', 'dbname');
+//define('DB_USERNAME_HW10', 'username');
+//define('DB_PW_HW10', 'password');
+//define('DB_HOSTNAME', 'hostname');
+//define('DB_NAME_HW10', 'dbname');
 
 //encoding
 mysqli_set_charset ($connection, 'utf8');
@@ -26,31 +26,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     if (empty ($_POST['first_name'])) {
-        $errors[] = 'You forgot to enter your first name';
+        $errors['first_name'] = 'You forgot to enter your first name';
     } else {
         $fn = trim($_POST['first_name']);
     }
 
     if (empty ($_POST['last_name'])) {
-        $errors[] = 'You forgot to enter your last name';
+        $errors['last_name'] = 'You forgot to enter your last name';
     } else {
         $ln = trim($_POST['last_name']);
     }
 
     if (empty ($_POST['email'])) {
-        $errors[] = 'You forgot to enter your email address';
+        $errors['email'] = 'You forgot to enter your email address';
     } else {
         $e = trim($_POST['email']);
     }
 
-    if (!empty ($_POST['password_og'])) {
-        if ($_POST['password_og'] != $_POST['password_confirm']) {
-            $errors[] = 'Your password does not match confirmation password';
+    if (!empty ($_POST['password'])) {
+        if ($_POST['password'] != $_POST['password_confirm']) {
+            $errors['password_confirm'] = 'Your password does not match confirmation password';
         } else {
-            $pw = trim($_POST['password_og']);
+            $pw = trim($_POST['password']);
         }
     } else {
-        $errors[] = 'You forgot to enter your password';
+        $errors['password'] = 'You forgot to enter your password';
     }
 
     if ( empty($errors)) {
@@ -58,7 +58,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 //Add query
 
-    $query = "INSERT INTO USER_PETTIGREW (first_name, last_name, email, password_og)
+    $query = "INSERT INTO USER_PETTIGREW (first_name, last_name, email, password)
     VALUES ('$fn', '$ln', '$e', '$pw')";
     $r = @mysqli_query($connection, $query);
     if ($r) {
@@ -75,18 +75,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
     mysqli_close($connection);
 
-    exit();
+    //exit();
 
     } else {
         echo'<h1>Error!</h1>
-        <p class="error">The following error(s) occurred:<br>';
-    foreach ($errors as $msg) {
-        echo " - $msg<br>\n";
-      }
+       <p class="error"><br>';
+    //foreach ($errors as $msg) {
+        //echo " - $msg<br>\n";
+      //}
     echo '</p><p>Please try again.</p><p><br></p>';
 
     }
-}
+//}
 /*
 *   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
 */
@@ -115,41 +115,47 @@ if($result) {
         <label for="first_name">First Name</label>
         <input type="text" id="first_name" name="first_name" value="<?php if (isset($_POST['first_name'])) //or <?php echo $first_name; //end php>
          echo $_POST['first_name'];?>"><br>
+         <?php if (isset($errors['first_name'])) echo $errors['first_name'];?><br>
 
 
         <label for="last_name">Last Name</label>
         <input type="text" id="last_name" name="last_name" value="<?php if (isset($_POST['last_name']))
          echo $_POST['last_name'];?>"><br>
+         <?php if(isset($errors['last_name'])) echo $errors['last_name']; ?><br>
 
         <label for="email">Email</label>
         <input type="email" id="email" name="email" value="<?php if (isset($_POST['email']))
          echo $_POST['email'];?>"><br>
+         <?php if (isset($errors['email'])) echo $errors['email'];?><br>
 
         <label for="password">Password</label>
         <input type="password" id="password" name="password" value="<?php if (isset($_POST['password']))
          echo $_POST['password'];?>"><br>
+         <?php if (isset($errors['password'])) echo $errors['password'];?><br>
 
         <!--Add a second password input so the user has to retype their password -->
 
         <label for="password_confirm">Confirm Password</label>
         <input type="password" id="password_confirm" name="password_confirm" value="<?php if (isset($_POST['password_confirm']))
          echo $_POST['password_confirm'];?>"><br>
+         <?php if (isset($errors['password_confirm'])) echo $errors['password_confirm'];?><br>
 
-         <input type="hidden" name="user_id" value=",?php echo $id?>"><br>
+         <!--<input type="hidden" name="user_id" value="<?php //echo $id; ?>"><br>-->
 
         <button>Register</button>
     </form>
 
 <?php
 //added ../ to database.php
-    require('database.php');  //connect to the db
-    $query = "SELECT (user_id, first_name, last_name, email, password_og)  //why is user_id YELLOW?
+    //require('database.php');  //connect to the db
+      // why is user_id YELLOW?
+    $query = "SELECT (user_id, first_name, last_name, email, password)
             FROM USER_PETTIGREW
             ORDER BY first_name ASC";
-    $r = @mysqli_query($connection, $query);  //runs teh query from db table USER_PETTIGREW
+    $r = @mysqli_query($connection, $query);  //runs the query from db table USER_PETTIGREW
 
 
-    <h2>Output a List of Users</h2>
+    echo '<h2>Output a List of Users</h2>';
 
     if ($r) {
 
@@ -185,8 +191,8 @@ if($result) {
         mysqli_free_result($r);
 
 } else {
-    echo'<p class="error"> The current users could not be retrieved.  We apologize for any inconvenience.</p>';
-    echo '<p>' . mysqli_error($connection) . '<br><br>Query: ' . $query . '</p>';
+    echo '<p class="error"> The current users could not be retrieved.  We apologize for any inconvenience.</p>';
+
 }
 
 mysqli_close($connection);
