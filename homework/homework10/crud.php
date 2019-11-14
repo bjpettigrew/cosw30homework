892e12821a0df44cca7600de39026fa421cb3591
bjpettigrew <bjpettigrew@gmail.com>
2019-11-14 00:38:19 +0000

    change edit to user_id in link in table

--- homework/homework10/crud.php
+++ homework/homework10/crud.php
@@ -239,7 +239,7 @@ if($result) {
                 <td align="left">' . $row['last_name'] . '</td>
                 <td align="left">' . $row['email'] . '</td>
                 <td align="left">' . $row['password'] . '</td>
-                <td align="left"><a href="update.php?id=' .$row['edit']. '"><p class="update">Update</p></a></td></tr>
+                <td align="left"><a href="update.php?id=' .$row['user_id']. '"><p class="update">Update</p></a></td></tr>
             ';
             }
         echo '</tbody></table>';
